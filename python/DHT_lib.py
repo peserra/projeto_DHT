1e62aa8f4e82077703f21a84ca5f08a56c1ae63c
peserra <pedro.occulate@gmail.com>
2025-05-21 11:25:43 -0300

    adding uv

--- python/DHT_lib.py
+++ python/DHT_lib.py
@@ -4,8 +4,6 @@ import dht_pb2
 import dht_pb2_grpc
 import hashlib
 from concurrent import futures
-import time
-import threading
 import asyncio
 import traceback
 
@@ -18,21 +16,21 @@ class Item:
 # classe que vai gerenciar a dht e nodes
 class DhtManager(dht_pb2_grpc.DhtOperationsServicer):
     def __init__(self) -> None:
-        self.known_hosts:list = []
-    
+        self.known_hosts:list = [] # type: ignore
+
     class Node:
         def __init__(self, ip_addr:str, port:int) -> None:
-            self.id:str      = f"{ip_addr}:{port}" 
+            self.id:str      = f"{ip_addr}:{port}"
             self.ip_addr:str = ip_addr
             self.port:int    = port
             #self.id_hash:str = hashlib.sha256(self.id.encode(encoding="utf-8")).hexdigest()
             self.id_hash:str = str(self.port) # so para testar mesmo
             # valor padrao de um novo node eh ser seu proprio next e prev, antes de entrar na dht
-            self.id_next:str = self.id 
+            self.id_next:str = self.id
             self.id_prev:str = self.id
             self.stored_items:dict = {}
             self.ready_event = asyncio.Event()
-        
+
         # quero estabelecer a conexao com o primeiro node da lista de nodes conhecidos
         async def init_node_stub(self, node_id:str):
             # cria uma conexao com o seu next para mandar a mensagem pra ele
@@ -40,7 +38,7 @@ class DhtManager(dht_pb2_grpc.DhtOperationsServicer):
             node_stub = dht_pb2_grpc.DhtOperationsStub(channel)
             print(f"stub criado no canal {node_id}")
             return node_stub
-            
+
         # inicializa um listener do node, com seu proprio id que escuta por 5s
         async def init_node_listening(self):
             port = str(self.port)
@@ -56,33 +54,33 @@ class DhtManager(dht_pb2_grpc.DhtOperationsServicer):
             except asyncio.CancelledError:
                 print(f"listener de {self.id} fechado")
 
-    
+
     '''
         func deve receber o node que quer entrar e mandar infos dele para o next na dht, essa operacao deve ser chamada toda vez ate encontrar o local correto do node.
     '''
     def FindNext(self, request, context) -> None:
         print("conexao funcionou pelo menos")
         return super().FindNext(request, context)
-    
-    
+
+
     '''
         - Usada no momento que quiser entrar na DHT
         - Recebe como param uma lista de nos conhecidos pela rede (pode ler de uma lista caso
             seja primeiro)
         - caso nao consiga encontar ninguem participando, ele é o primeiro
-        
+
 
         - ao fim dessa funcao, devem ter sido atualizados:
             * os campos id_next e id_prev do no ingressante
             * o campo id_prev do sucessor do no ingressante
             * o campo id_next do predecessor do ingressante
-        
+
         - devem tambem ser transferidos do sucessor os dados que o no ingressante deverá cuidar (mensagem Transfer)
     '''
     async def join(self, node:Node):
         if self.known_hosts:
             print("tinha dht, adicionando novo node")
-            
+
             entry_node = self.known_hosts[0]
             # funcao de server do node vai ficar ouvindo por conexao
             # esta ouvindo em "localhost:1234"
@@ -98,18 +96,18 @@ class DhtManager(dht_pb2_grpc.DhtOperationsServicer):
                     print("RETORNOU O STUB")
                     node_stub.FindNext(dht_pb2.Join(joining_node=dht_pb2.NodeInfo(id=node.id_hash,
                                                                            ip_addr=node.ip_addr, port=node.port)))
-                    
+
             except Exception as e:
                 print(f"Erro ao chamar a funcao FindNext: {e}")
                 traceback.print_exc()  # Mostra o stack trace completo para ajudar a identificar o problema
-        
+
         # adiciona na lista de hosts conhecidos da dht
         self.known_hosts.append(node)
 
     def leave(self, node:Node):
         '''
             - Usada para sair da rede
-            
+
             - Ao fim dessa chamada:
                 * ponteiros dos vizinhos devem ter sido devidamente atualizados
                 * conteudo deve ter sido repassado ao no sucessor (mensagem Transfer)
@@ -131,7 +129,7 @@ class DhtManager(dht_pb2_grpc.DhtOperationsServicer):
         '''
             - Armazena o 'value' utilziando a 'key' no dicionario stored_items do no
 
-            - o item deve ser armazenado no no responsavel pelo intervalo que contem valor 
+            - o item deve ser armazenado no no responsavel pelo intervalo que contem valor
             calcHash(key)
         '''
         # Calcula o hash da chave
@@ -181,7 +179,7 @@ class DhtManager(dht_pb2_grpc.DhtOperationsServicer):
             next_node_stub = current_node.init_node_stub(current_node.id_next)
             response = next_node_stub.RetrieveItem(dht_pb2.Retrieve(key=key, searching_node=dht_pb2.NodeInfo(
                 id=current_node.id_hash, ip_addr=current_node.ip_addr, port=current_node.port)))
-            
+
             if response.HasField("Ok"):
                 # Retorna o valor recebido da resposta
                 return response.value.decode()
@@ -189,10 +187,10 @@ class DhtManager(dht_pb2_grpc.DhtOperationsServicer):
                 print(f"Chave {key} não encontrada em toda a DHT")
                 return None
 
-            
 
-        
-    
+
+
+
 
 def CreateDhtNode(ip_addr:str, port:int) -> DhtManager.Node:
     return DhtManager.Node(ip_addr=ip_addr, port=port)
